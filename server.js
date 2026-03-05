2b68d2035cc025f0a36a2c93b6982bb544b9f3d6
mostafaahmedelderei <mostafa.ahmed.elderei@gmail.com>
2026-03-06 01:13:43 +0200

    ADD: category and product database then controllers and routes with test

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 import dotenv from "dotenv"
 dotenv.config();
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import { dbConnect } from "./Database/dbConnect.js";
+
+import productRoute from "./Routes/product.route.js";
+import categoryRoute from "./Routes/category.route.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 await dbConnect(); 
 
 const app = express();
 app.use(express.json());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/categories", categoryRoute);
+app.use("/products", productRoute);
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
 
 
 app.listen(PORT, () => {
