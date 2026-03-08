08975d62aa75e2a988b82683198ba7aadfbe7d6a
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-03-09 00:27:37 +0200

    added forgot password

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -4,26 +4,36 @@ import { sendEmail } from "../Utils/Email/sendEmail.js";
 import User from "../Models/user.model.js";
 import catchAsyncError from "../Utils/catchAsyncError.js";
 import AppErrors from "../Utils/appErrors.js";
+import crypto from "crypto";
 
 const signToken = (id, role, email) => {
   return jwt.sign({ id, role, email }, process.env.JWT_SECRET);
 };
 
+const generateRestToken = () => {
+  const randomBytes = crypto.randomBytes(32).toString("hex");
+  return crypto
+    .createHash('sha256')
+    .update(randomBytes)
+    .digest('hex');
+}
+
 export const register = catchAsyncError(async (req, res, next) => {
-    const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-      phone: req.body.phone
-    });
-    const url = `${req.protocol}://${req.get('host')}/api/v1/users/verify`;
-    await sendEmail(newUser.email, url);
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      data: newUser
-    });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    phone: req.body.phone
+  });
+  newUser.password = undefined;
+  const url = `${req.protocol}://${req.get('host')}/api/v1/users/verify`;
+  await sendEmail(newUser.email, url);
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    data: newUser
+  });
 });
 
 export const login = catchAsyncError(async (req, res, next) => {
@@ -38,20 +48,48 @@ export const login = catchAsyncError(async (req, res, next) => {
 
 
 export const logout = catchAsyncError(async (req, res, next) => {
-
+  res.send("Logout");
 });
 
 
 export const forgotPassword = catchAsyncError(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppErrors("User not found", 404));
+  }
+  const restToken = generateRestToken();
+
+  const url = `${req.protocol}://${req.get('host')}/api/v1/users/reset-password/${restToken}`;
+  user.passwordResetToken = restToken;
+  user.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  await user.save({ validateBeforeSave: false });
+
+  try {
+    await sendEmail(user.email, url);
+    res.status(200).json({
+      success: true,
+      message: "Email sent successfully"
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new AppErrors("Email could not be sent to user", 500));
+  }
 
 });
 
 export const resetPassword = catchAsyncError(async (req, res, next) => {
-
+  const token = req.params.token;
+  res.send({
+    token
+  });
 });
 
 export const updatePassword = catchAsyncError(async (req, res, next) => {
-
+  res.send({
+    message: "Password updated successfully"
+  })
 });
 
 
