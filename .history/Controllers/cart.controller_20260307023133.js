5833eef65959500ca0d33a62a4e622da9e131777
alaaateff <alaaatef3200@gmail.com>
2026-03-09 03:21:32 +0200

    delete .history folder

--- .history/Controllers/cart.controller_20260307023133.js
+++
@@ -1,7 +0,0 @@
-import User from "../Models/user.model.js"
-
-export const add_items = async(req,res) =>{
-        console.log(req.body)
-        let addcart = await User.create(req.body)
-        res.json
-}
--- .history/Controllers/cart.controller_20260308211643.js
+++
@@ -1,9 +0,0 @@
-import User from "../Models/user.model.js"
-import jwt from "jsonwebtoken"
-
-export const add_items = async(req,res) =>{
-        let token  = req.headers.token;
-        try {}
-
-        
-}
