a3e34769daa37135971c9c13ce3d65cd49a6f7bb
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-03-13 21:58:24 +0200

    added reviews

--- Routes/product.route.js
+++ Routes/product.route.js
@@ -8,16 +8,32 @@ import {
     deleteProduct
 } from "../Controllers/product.controller.js";
 
+import {
+    createReview,
+    getAllProductReviews,
+    deleteReview,
+    updateReview
+} from "../Controllers/review.controller.js";
+
+import { protect } from "../Middlewares/protect.js";
+import { chekcReviewExistAndOwnership } from "../Middlewares/reviewMiddleware.js";
 const router = express.Router();
 
+router.use(protect);
+
 router.post("/", addProduct);
-
 router.get("/", getProducts);
-
 router.get("/:id", getProduct);
-
 router.put("/:id", updateProduct);
-
 router.delete("/:id", deleteProduct);
+
+router.route("/:product_id/reviews")
+        .post(createReview)    
+        .get(getAllProductReviews);
+
+router.route("/:product_id/reviews/:review_id")
+        .delete(chekcReviewExistAndOwnership,deleteReview)
+        .put(chekcReviewExistAndOwnership, updateReview);
+
 
 export default router;
