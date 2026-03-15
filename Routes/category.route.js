ff8af068263e1698acfba852540e0de9147db5b0
Ahmed Bakry <ahmedbakry024@gmail.com>
2026-03-16 00:22:52 +0200

    Middleware Added

--- Routes/category.route.js
+++ Routes/category.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { addCategory, getCategories, deleteCategory } from "../Controllers/category.controller.js";
-import {  isAdmin } from "../Middlewares/protect.js";
+import { protect ,isAdmin } from "../Middlewares/protect.js";
 
 const router = express.Router();
 
-router.post("/", isAdmin, addCategory);
+router.post("/", protect, isAdmin, addCategory);
 
 router.get("/", getCategories);
 
-router.delete("/:id", isAdmin, deleteCategory);
+router.delete("/:id", protect, isAdmin, deleteCategory);
 
 export default router;
